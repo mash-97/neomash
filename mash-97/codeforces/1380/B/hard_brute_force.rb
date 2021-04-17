c9e1de38f331f8955f12c6f24101276617d74136
mash-97 <mahimnhd97@gmail.com>
2021-04-18 00:53:37 +0600

    adds brute_force solutions, ruby-thor-based cli program for test
    hard_brute_force solution and a cli program to test the program from input file. For the purpose of helping to develop an efficient solution to the problem, this brute force program was written

--- mash-97/codeforces/1380/B/hard_brute_force.rb
+++ mash-97/codeforces/1380/B/hard_brute_force.rb
@@ -1,1 +1,91 @@
 
+# defines a public method n_perm which produces (elements.length)**(positions)
+# permutations for elements
+# as an instance method to the array
+class Array
+  @n_perms = []
+  def n_perm(p)
+    @n_perms = []
+    _n_perm_(self, p)
+    return @n_perms
+  end
+  
+  
+  private
+  def _n_perm_(elements_array, p, current_pos=0, temp_array=[])
+    if current_pos == p then
+      @n_perms << temp_array.clone
+      return 
+    end
+    
+    elements_array.each do |element|
+      temp_array << element
+      _n_perm_(elements_array, p, current_pos+1, temp_array)
+      temp_array.pop()
+    end
+  end
+end
+
+
+# Hard Brute Force: because it just simply does the what the questions say
+# For Codeforces/1380/B "Universal Solution" problem 
+class UniversalSolution 
+  
+  # counts average wins for a given choices ca against the bot's sa
+  def average_wins(sa, ca, n)
+    wins = 0.0
+    n.times do |nni|
+      sub_wins = 0
+      n.times do |ni|
+	sub_wins += yield(sa[(nni+ni)%n], ca[ni]) == true ? 1 : 0;
+      end
+      wins += sub_wins.to_f
+    end
+    return wins/n
+  end
+  
+  # simulation part
+  def universal_solutions(sa)
+    ups = ['R', 'P', 'S'].n_perm(sa.length())
+    solutions = {}
+    avw = -1.0
+    
+    ups.each do |up|
+      temp_avw = average_wins(sa, up, sa.length) do |s,c|
+	if (s=='R' and c=='P') or (s=='P' and c=='S') or (s=='S' and c=='R') then
+	  true
+	else
+	  false
+	end
+      end
+      
+      if temp_avw == avw 
+	solutions[temp_avw] << up
+	
+      elsif temp_avw > avw 
+	solutions[temp_avw] = [up]
+	avw = temp_avw
+      end
+    end
+    
+    return [solutions, avw]
+  end
+  
+  def start(max_show=10)
+    t = gets.to_i()
+    t.times do 
+      s = gets.chomp!
+      s = s.split('')
+      result_hash, max_av = universal_solutions(s)
+      puts("max_average value is: #{max_av}")
+      puts("Possible solutions: ")
+      result_hash[max_av][0..max_show].each do |solution|
+	puts("\t#{solution.to_s}")
+      end
+      puts()
+    end
+  end
+  
+end
+
+
---
+++ mash-97/codeforces/1380/B/hbcli.rb
@@ -0,0 +1,28 @@
+require "thor"
+require_relative("hard_brute_force")
+
+class UniversalSolutionCli < Thor
+  desc "usf INPUT_FILE", "run universal solutions on input file INPUT_FILE"
+  option :max_show, type: :numeric, default: 10
+  def usf(input_file)
+    input_file = File.open(input_file, "r")
+    input_lines = input_file.readlines()
+    
+    t = input_lines[0].to_i
+    t.times do |i|
+      s = input_lines[i+1]
+      puts("Bot String: #{s}")
+      s = s.split('')
+      us = UniversalSolution.new()
+      result_hash, max_av = us.universal_solutions(s)
+      puts("max_average value is: #{max_av}")
+      puts("Possible solutions(#{result_hash[max_av].length()} available | showing #{options[:max_show]}): ")
+      result_hash[max_av][0..options[:max_show]].each do |solution|
+	puts("\t#{solution.to_s} ==>\t#{solution.join()}")
+      end
+      puts()
+    end
+  end
+end
+
+UniversalSolutionCli.start(ARGV)
