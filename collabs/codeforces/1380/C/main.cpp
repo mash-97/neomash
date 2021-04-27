ba43859bacef16570b3edd718e91441cb59e800c
mash-97 <mahimnhd97@gmail.com>
2021-04-28 01:33:08 +0600

    towards simulation of codeforces:1380:C

---
+++ collabs/codeforces/1380/C/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+// custom header files
+#include <helper.h>
+#include <utils.h>
+
+
+// You code goes below
+
+
+int main(){
+
+    return 0;
+}
