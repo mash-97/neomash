d5a2cc8c191c0fafc04fa0a122ae925748b5ba86
mash-97 <mahimnhd97@gmail.com>
2021-05-11 21:29:33 +0600

    mash:97: cfc1380D -> still on wrong ans on 7

--- mash-97/codeforces/1380/D/solution_D.py
+++ mash-97/codeforces/1380/D/solution_D.py
@@ -23,14 +23,40 @@ def does_lkmin_exist(seq, si, ei, lk, lbn=-1, rbn=-1):
         return True
     return False
 
-def get_energy_by_xmym(seq, si, ei, x, k, y, lbn=-1, rbn=-1):
-    
-    if (ei-si) < k:
+def get_energy_by_xm(seq, si, ei, x, k, y, lbn=-1, rbn=-1):
+    l = (ei-si)
+    if l < k:
         return PINF
-    if (ei-si) > k and not does_lkmin_exist(seq, si, ei, ((ei-si)%k), lbn, rbn):
+    elif (l%k)>0 and not does_lkmin_exist(seq, si, ei, l%k, lbn, rbn):
         return PINF
     
-    return ((ei-si)//k)*x + ((ei-si)%k)*y
+    return (l//k)*x + (l%k)*y
+
+
+def count_mnmx(seq, si, ei, x, k, y, lbn=-1, rbn=-1):
+    mn = 0
+    mx = 0
+    lmr = max([lbn, rbn])
+    
+    for i in range(si, ei):
+        if seq[i]>lmr:
+            mx += 1
+        if seq[i]<lmr:
+            mn += 1
+    return [mn, mx]
+
+
+def get_energy_by_ymxm(seq, si, ei, x, k, y, lbn=-1, rbn=-1):
+    mn,mx = count_mnmx(seq, si, ei, x, k, y, lbn, rbn)
+    if mx!=0 and mx%k!=0:
+        inc = ((mx+k)-(mx%k)) - mx
+        if inc > mn:
+            return PINF
+        mx += inc
+        mn -= inc
+    
+    return (mx//k)*x + mn*y
+
 
 def get_energy_by_ym(seq, si, ei, y, lbn=-1, rbn=-1):
     if does_maxn_exist(seq, si, ei, lbn, rbn):
@@ -39,24 +65,26 @@ def get_energy_by_ym(seq, si, ei, y, lbn=-1, rbn=-1):
 
 
 def get_min_energy(seq, si, ei, x, k, y, lbn=-1, rbn=-1):
-    exm = get_energy_by_xmym(seq, si, ei, x, k, y, lbn, rbn)
+    exm = get_energy_by_xm(seq, si, ei, x, k, y, lbn, rbn)
     # ~ print("si: {}, ei: {}, exm: {}".format(si, ei, exm))
+    eymxm = get_energy_by_ymxm(seq, si, ei, x, k, y, lbn, rbn)
     
     eym = get_energy_by_ym(seq, si, ei, y, lbn, rbn)
     # ~ print("si: {}, ei: {}, eym: {}".format(si, ei, eym))
+    # ~ print("exm: {}, eymxm: {}, eym: {}".format(exm, eymxm, eym))
     
-    if(min([exm, eym])==PINF):
+    if(min([exm, eym, eymxm])==PINF):
         return -1
-    return min([exm, eym])
+    return min([exm, eym, eymxm])
 
 
-def check_bseq(a, b, m, ia):
-    i = 1
-    while i<m:
-        if ia[b[i-1]] > ia[b[i]]:
-            return False
-        i += 1
-    return True
+# ~ def check_bseq(a, b, m, ia):
+    # ~ i = 1
+    # ~ while i<m:
+        # ~ if ia[b[i-1]] > ia[b[i]]:
+            # ~ return False
+        # ~ i += 1
+    # ~ return True
 
 
 def main():
@@ -69,6 +97,7 @@ def main():
     b = list(map(intifier, input().split(" ")))
     
     ia = [-1 for i in range(n+1)]
+    
     for i, ai in enumerate(a):
         ia[ai] = i    
     
@@ -82,7 +111,10 @@ def main():
     
     if ia[b[0]] > 0:
         tmp_e = get_min_energy(a, 0, ia[b[0]], x, k, y, -1, b[0])
-        energy += 0 if tmp_e==-1 else tmp_e
+        if tmp_e==-1:
+            print("-1")
+            return None
+        energy+=tmp_e
         # ~ print("0-{}: {}".format(ia[b[0]], tmp_e))
     
     while indx < m:
@@ -91,7 +123,11 @@ def main():
                 energy += 0 
             else:
                 tmp_e = get_min_energy(a, ia[b[indx-1]]+1, ia[b[indx]], x, k, y, b[indx-1], b[indx])
-                energy += 0 if tmp_e==-1 else tmp_e
+            
+                if tmp_e==-1:
+                    print("-1")
+                    return None
+                energy+=tmp_e
         
         else:
             print("-1")
@@ -101,7 +137,10 @@ def main():
     
     if ia[b[m-1]] < n-1:
         tmp_e = get_min_energy(a, ia[b[m-1]]+1, n, x, k, y, b[m-1], -1)
-        energy += 0 if tmp_e==-1 else tmp_e
+        if tmp_e==-1:
+            print("-1")
+            return None
+        energy+=tmp_e
         # ~ print("%s-..: %s"%(m-1, tmp_e))
     print(energy)
 
@@ -109,4 +148,19 @@ def main():
 
 if __name__=="__main__":
     main()
-    
+
+
+"""
+5 3
+2 1 2
+5 2 8 1 3    
+5 3
+"""
+
+"""
+5 2
+2 1 1
+4 5 3 2 1
+4 1
+
+"""
